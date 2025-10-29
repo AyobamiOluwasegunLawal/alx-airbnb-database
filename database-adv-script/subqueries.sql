3cec1e598fa27cb3499d95bb37b3894596a909c4
AyobamiOluwasegunLawal <ayobamioluwasegslawal@gmail.com>
2025-10-29 10:04:38 +0100

    adjust case

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -4,7 +4,7 @@ WHERE (
 	SELECT AVG(r.rating)
 	FROM Review AS r
 	WHERE r.property_id = p.property_id
-) > 4
+) > 4.0
 
 
 SELECT u.first_name, u.lASt_name, u.email
@@ -13,4 +13,4 @@ WHERE (
 	SELECT COUNT(*)
 	FROM Booking AS b
 	WHERE b.user_id = u.user_id
-) > 1
+) > 3
