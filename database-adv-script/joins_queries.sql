49686385caf87518b8bcb70c9103a44c0064ff72
AyobamiOluwasegunLawal <ayobamioluwasegslawal@gmail.com>
2025-10-29 06:18:20 +0100

    correction

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -4,19 +4,20 @@ ON Users.user_id = Booking.user_id
 
 SELECT *
 FROM Property LEFT JOIN Review
-ON Property.property_id = Review.property_id
+ON Property.property_id = Review.property_id;
 
 SELECT *
 FROM Users FULL OUTER JOIN Booking
-ON Users.user_id = Booking.user_id
+ON Users.user_id = Booking.user_id;
 
 SELECT *
 FROM Users LEFT JOIN Booking
 ON Users.user_id = Booking.user_id
+ORDER BY Users.first_name ASC, Booking.price DESC;
 
 UNION
 -- if you want duplicates use 'union all'
 
 SELECT *
 FROM Users RIGHT JOIN Booking
-ON Users.user_id = booking.user_id
+ON Users.user_id = booking.user_id;
