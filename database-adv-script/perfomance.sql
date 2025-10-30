d4f35077043631e3c1d8c1c212ea20a12a8556ce
AyobamiOluwasegunLawal <ayobamioluwasegslawal@gmail.com>
2025-10-30 13:16:34 +0100

    done

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,7 +1,7 @@
-SET STATISTICS IO ON;
-SET STATISTICS TIME ON;
+EXPLAIN ANALYZE
 SELECT * FROM Booking AS b
 JOIN Users AS u
 ON u.user_id = b.user_id
 JOIN Property AS p
 ON b.property_id = p.property_id
+WHERE p.location = 'New York' AND b.start_date >= '2023-01-01';
