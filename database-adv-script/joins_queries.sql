9a4193a5b676f4d667632bbce0b4d339b4f92d41
AyobamiOluwasegunLawal <ayobamioluwasegslawal@gmail.com>
2025-10-30 08:39:26 +0100

    done

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,3 +1,11 @@
 SELECT * FROM Users AS u INNER JOIN Booking AS b ON u.user_id = b.user_id;
-SELECT * FROM Property  AS p LEFT JOIN  Reviews AS r ON p.user_id = r.user_id;
+SELECT * FROM Property  AS p LEFT JOIN Review AS r ON p.property_id = r.property_id;
 SELECT * FROM Users AS u FULL OUTER JOIN Booking AS b ON u.user_id = b.user_id;
+
+SELECT * FROM Users AS u
+LEFT JOIN Booking AS b ON u.user_id = b.user_id
+
+UNION
+
+SELECT * FROM Users AS u
+RIGHT JOIN Booking AS b ON u.user_id = b.user_id;
