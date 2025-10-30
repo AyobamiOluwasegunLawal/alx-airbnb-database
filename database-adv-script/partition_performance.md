81c044235cd9f242e9c701d74d197781695cbc20
AyobamiOluwasegunLawal <ayobamioluwasegslawal@gmail.com>
2025-10-30 14:00:00 +0100

    new changes

---
+++ database-adv-script/partition_performance.md
@@ -0,0 +1,1 @@
+This contains the performance of the sql query optimization
--- database-adv-script/partitioning.sql
+++ database-adv-script/partitioning.sql
@@ -1,4 +1,18 @@
-CREATE PARTITION FUNCTION PartitionByStartDate(DATE)
-AS RANGE LEFT FOR VALUES('2023-12-31', '2024-12-31', '2025-12-31');
-
-
+CREATE TABLE PartitionByYears (
+    booking_id CHAR(36) NOT NULL,
+    property_id CHAR(36) NOT NULL,
+    user_id CHAR(36) NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    total_price DECIMAL(10,2) NOT NULL,
+    status VARCHAR(20) NOT NULL CHECK(status IN ('pending', 'confirmed', 'cancelled')),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (booking_id, start_date)
+)
+PARTITION BY RANGE (YEAR(start_date)) (
+    PARTITION p2023 VALUES LESS THAN (2024),
+    PARTITION p2024 VALUES LESS THAN (2025),
+    PARTITION p2025 VALUES LESS THAN (2026),
+    PARTITION p2026 VALUES LESS THAN (2027),
+    PARTITION p_future VALUES LESS THAN MAXVALUE
+);
